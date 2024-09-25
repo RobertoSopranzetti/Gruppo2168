86d86a50f8480e5b6331c05efbb37505a8891e13
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-09-25 23:28:02 +0200

    Refactor Admin DAO to add methods for signing up admins and performing user moderation

--- app/src/main/java/db_lab/data/Admin.java
+++ app/src/main/java/db_lab/data/Admin.java
@@ -2,67 +2,75 @@ package db_lab.data;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public final class Admin {
-
-    public final String username;
-    public final String password;
-    public final String email;
-    public final String firstName;
-    public final String lastName;
-
-    public Admin(String username, String password, String email, String firstName, String lastName) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof Admin)) {
-            return false;
-        }
-        Admin other = (Admin) obj;
-        return this.username.equals(other.username) &&
-                this.password.equals(other.password) &&
-                this.email.equals(other.email) &&
-                this.firstName.equals(other.firstName) &&
-                this.lastName.equals(other.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.username, this.password, this.email, this.firstName, this.lastName);
-    }
-
-    @Override
-    public String toString() {
-        return Printer.stringify("Admin", List.of(
-                Printer.field("username", this.username),
-                Printer.field("password", this.password),
-                Printer.field("email", this.email),
-                Printer.field("firstName", this.firstName),
-                Printer.field("lastName", this.lastName)));
-    }
+public record Admin() {
 
     public final class DAO {
 
-        public static void signUpAdmin(Connection connection, String username, String password, String nome,
-                String cognome, String email) {
-            try (PreparedStatement statement = connection.prepareStatement(Queries.ADMIN_SIGN_UP)) {
-                statement.setString(1, username);
-                statement.setString(2, password);
-                statement.setString(3, nome);
-                statement.setString(4, cognome);
-                statement.setString(5, email);
+        public static void signUpAdmin(Connection connection, String username, String password, String email) {
+            try (PreparedStatement statement = DAOUtils.prepare(connection, Queries.ADMIN_SIGN_UP, username, password,
+                    email)) {
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static List<String> badUsers(Connection connection) {
+            List<String> result = new ArrayList<>();
+            String query = Queries.SHOW_BAD_USERS;
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        result.add(resultSet.getString("Username"));
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        public static List<String> reportedUsers(Connection connection) {
+            List<String> result = new ArrayList<>();
+            String query = Queries.SHOW_REPORTED_USERS;
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        result.add(resultSet.getString("Username"));
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        public static void userModeration(Connection connection, String adminName, String username, String tipo) {
+            try (PreparedStatement statement = DAOUtils.prepare(connection, Queries.USER_MODERATION, adminName,
+                    username, tipo, username)) {
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void addSubcategory(Connection connection, String nome, String descrizione, String username) {
+            try (PreparedStatement statement = DAOUtils.prepare(connection, Queries.ADD_SUBCATEGORY, nome, descrizione,
+                    username)) {
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void addToSubcategory(Connection connection, int idInserzione, String subcategoryName) {
+            try (PreparedStatement statement = DAOUtils.prepare(connection, Queries.ADD_TO_SUBCATEGORY, idInserzione,
+                    subcategoryName)) {
                 statement.executeUpdate();
             } catch (SQLException e) {
                 e.printStackTrace();
